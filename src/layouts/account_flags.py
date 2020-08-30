e9be8d295390ab200e736d5847c0863214ec35c2
michaelhly <michaelhly@gmail.com>
2020-08-30 18:40:17 -0500

    Replace empty flags with padding

--- src/layouts/account_flags.py
+++ src/layouts/account_flags.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-from construct import BitStruct, BitsSwapped, Flag  # type: ignore
+from construct import BitsInteger, BitStruct, BitsSwapped, Flag  # type: ignore
 
 _EMPTY_FLAG = ""
 
@@ -14,63 +14,7 @@ _ACCOUNT_FLAGS_LAYOUT = BitsSwapped(  # Swap to little endian
         "eventQueue" / Flag,
         "bids" / Flag,
         "asks" / Flag,
-        _EMPTY_FLAG / Flag,  # XXX: I can't get Padding to work correctly. # pylint: disable=fixme
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
-        _EMPTY_FLAG / Flag,
+        _EMPTY_FLAG / BitsInteger(57),
     )
 )
 
@@ -80,5 +24,5 @@ def decode_account_flags(raw_flags: bytes) -> Dict:
 
 
 def encode_account_flags(flag_params: Dict) -> bytes:
-    flag_params[None] = False  # XXX: To take care of empty flags?  pylint: disable=fixme
+    flag_params[None] = False  # Set empty flag
     return _ACCOUNT_FLAGS_LAYOUT.build(flag_params)
