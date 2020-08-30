89014c628799c7fb4a2a2ac0df2f345f0224ce4d
Michael Huang <michaelhly@gmail.com>
2020-08-30 18:45:39 -0500

    Account flags layout (#9)
    * Account flag layout
    
    * Add wrapper methods
    
    * Replace empty flags with padding

---
+++ src/layouts/account_flags.py
@@ -0,0 +1,28 @@
+from typing import Dict
+
+from construct import BitsInteger, BitStruct, BitsSwapped, Flag  # type: ignore
+
+_EMPTY_FLAG = ""
+
+# We will use a bitstruct with 64 bits instead of the widebits implementation in serum-js.
+_ACCOUNT_FLAGS_LAYOUT = BitsSwapped(  # Swap to little endian
+    BitStruct(
+        "initialized" / Flag,
+        "market" / Flag,
+        "openOrders" / Flag,
+        "requestQueue" / Flag,
+        "eventQueue" / Flag,
+        "bids" / Flag,
+        "asks" / Flag,
+        _EMPTY_FLAG / BitsInteger(57),
+    )
+)
+
+
+def decode_account_flags(raw_flags: bytes) -> Dict:
+    return _ACCOUNT_FLAGS_LAYOUT.parse(raw_flags)
+
+
+def encode_account_flags(flag_params: Dict) -> bytes:
+    flag_params[None] = False  # Set empty flag
+    return _ACCOUNT_FLAGS_LAYOUT.build(flag_params)
