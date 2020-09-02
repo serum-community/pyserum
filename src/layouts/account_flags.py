d732fda69a6511d669110dbb80e07b92b6ed76be
michaelhly <michaelhly@gmail.com>
2020-09-02 18:26:44 -0500

    Remove unnecessary helpers for account flags

--- src/layouts/account_flags.py
+++ src/layouts/account_flags.py
@@ -1,6 +1,4 @@
-from typing import Dict
-
-from construct import BitsInteger, BitsSwapped, BitStruct, Flag  # type: ignore
+from construct import BitsInteger, BitsSwapped, BitStruct, Const, Flag  # type: ignore
 
 # We will use a bitstruct with 64 bits instead of the widebits implementation in serum-js.
 ACCOUNT_FLAGS_LAYOUT = BitsSwapped(  # Swap to little endian
@@ -12,17 +10,6 @@ ACCOUNT_FLAGS_LAYOUT = BitsSwapped(  # Swap to little endian
         "event_queue" / Flag,
         "bids" / Flag,
         "asks" / Flag,
-        BitsInteger(57),
+        Const(0, BitsInteger(57)),  # Padding
     )
 )
-
-
-def decode_account_flags(raw_flags: bytes) -> Dict:
-    """Parse account flags from bytes."""
-    return ACCOUNT_FLAGS_LAYOUT.parse(raw_flags)
-
-
-def encode_account_flags(flag_params: Dict) -> bytes:
-    """Serialize account flags to bytes."""
-    flag_params[None] = False  # Set padding to false
-    return ACCOUNT_FLAGS_LAYOUT.build(flag_params)
--- tests/test_account_flags_layout.py
+++ tests/test_account_flags_layout.py
@@ -1,6 +1,6 @@
 """Tests for account flags layout."""
 
-from src.layouts.account_flags import ACCOUNT_FLAGS_LAYOUT, decode_account_flags, encode_account_flags
+from src.layouts.account_flags import ACCOUNT_FLAGS_LAYOUT
 
 
 def default_flags():
@@ -22,20 +22,20 @@ def test_correct_size():
 
 def test_decode():
     """Test account flag layout parses."""
-    parsed = decode_account_flags(bytes(16))
+    parsed = ACCOUNT_FLAGS_LAYOUT.parse(bytes(16))
     assert not parsed.initialized
     assert parsed == default_flags()
 
     expected = default_flags()
     expected["initialized"] = True
     expected["market"] = True
-    parsed = decode_account_flags(bytes.fromhex("0300000000000000"))
+    parsed = ACCOUNT_FLAGS_LAYOUT.parse(bytes.fromhex("0300000000000000"))
     assert parsed == expected
 
     expected = default_flags()
     expected["initialized"] = True
     expected["open_orders"] = True
-    parsed = decode_account_flags(bytes.fromhex("0500000000000000"))
+    parsed = ACCOUNT_FLAGS_LAYOUT.parse(bytes.fromhex("0500000000000000"))
     assert parsed == expected
 
 
@@ -44,4 +44,4 @@ def test_encode():
     flags = default_flags()
     flags["initialized"] = True
     flags["asks"] = True
-    assert encode_account_flags(flags) == bytes.fromhex("4100000000000000")
+    assert ACCOUNT_FLAGS_LAYOUT.build(flags) == bytes.fromhex("4100000000000000")
