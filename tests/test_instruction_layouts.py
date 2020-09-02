f2addcd1d85e63bc698ccf15e9ad374eb9982779
michaelhly <michaelhly@gmail.com>
2020-09-02 18:12:59 -0500

    Add parsing tests

--- tests/test_instruction_layouts.py
+++ tests/test_instruction_layouts.py
@@ -1,4 +1,7 @@
 """Tests for instruction layouts."""
+from solana.publickey import PublicKey
+
+from src.enums import OrderType, Side
 from src.layouts.instructions import _VERSION, INSTRUCTIONS_LAYOUT, InstructionType
 
 
@@ -23,6 +26,21 @@ def test_parse_initialize_market():
     assert_parsed_layout(InstructionType.InitializeMarket, INSTRUCTIONS_LAYOUT.parse(raw), expected_args)
 
 
+def test_parse_new_order():
+    """Test parsing raw new order data."""
+    expected_args = {
+        "limit_price": 1,
+        "max_quantity": 2,
+        "client_id": 3,
+        "side": Side.Sell,
+        "order_type": OrderType.PostOnly,
+    }
+    raw = bytes.fromhex(
+        "00010000000100000001000000000000000200000000000000020000000300000000000000"
+    )  # Raw hex from serum.js
+    assert_parsed_layout(InstructionType.NewOrder, INSTRUCTIONS_LAYOUT.parse(raw), expected_args)
+
+
 def test_parse_match_orders():
     """Test parsing raw match orders data."""
     expected_args = {"limit": 1}
@@ -30,8 +48,36 @@ def test_parse_match_orders():
     assert_parsed_layout(InstructionType.MatchOrder, INSTRUCTIONS_LAYOUT.parse(raw), expected_args)
 
 
-def test_consume_events():
+def test_parse_consume_events():
     """Test parsing raw consume events data."""
     expected_args = {"limit": 1}
     raw = bytes.fromhex("00030000000100")  # Raw hex from serum.js
     assert_parsed_layout(InstructionType.ConsumeEvents, INSTRUCTIONS_LAYOUT.parse(raw), expected_args)
+
+
+def test_parse_cancel_order():
+    """Test parsing raw cancel order data."""
+    expected_args = {
+        "side": Side.Buy,
+        "order_id": (1234567890).to_bytes(16, "little"),
+        "open_orders_slot": 123,
+        "open_orders": bytes(PublicKey(123)),
+    }
+    raw = bytes.fromhex(
+        "000400000000000000d202964900000000000000000000000000000000"
+        "0000000000000000000000000000000000000000000000000000007b7b"
+    )  # Raw hex from serum.js
+    assert_parsed_layout(InstructionType.CancelOrder, INSTRUCTIONS_LAYOUT.parse(raw), expected_args)
+
+
+def test_parse_settle_funds():
+    """Test parsing raw settle funds data."""
+    raw = bytes.fromhex("0005000000")  # Raw hex from serum.js
+    assert_parsed_layout(InstructionType.SettleFunds, INSTRUCTIONS_LAYOUT.parse(raw), None)
+
+
+def test_parse_cancel_order_by_client_id():
+    """Test parsing raw cancel order data."""
+    expected_args = {"client_id": 123}
+    raw = bytes.fromhex("00060000007b00000000000000")  # Raw hex from serum.js
+    assert_parsed_layout(InstructionType.CancelOrderByClientID, INSTRUCTIONS_LAYOUT.parse(raw), expected_args)
