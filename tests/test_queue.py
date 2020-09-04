cc1db8ed2b4bc0717281e0eaf092b6b888d74311
Leonard Ge <gw980531@gmail.com>
2020-09-04 06:18:48 +0100

    Improved code quality.

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -2,6 +2,7 @@ import base64
 
 from src.queue_ import decode_event_queue
 
+
 def test_decode_event_queue():
     """Test order book parsing."""
     with open("tests/binary/event_queue_binary.txt", "r") as input_file:
@@ -11,12 +12,11 @@ def test_decode_event_queue():
         assert len(event_queue) == 99
         event_queue = decode_event_queue(data, 100)
         assert len(event_queue) == 100
-        e = event_queue[0]
-        assert not e.event_flags.fill
-        assert e.event_flags.out
-        assert e.event_flags.bid
-        assert not e.event_flags.maker
-        assert e.open_order_slot == 17
-        assert e.fee_tier == 0
-        assert e.native_fee_or_rebate == 0
-        
+        event = event_queue[0]
+        assert not event.event_flags.fill
+        assert event.event_flags.out
+        assert event.event_flags.bid
+        assert not event.event_flags.maker
+        assert event.open_order_slot == 17
+        assert event.fee_tier == 0
+        assert event.native_fee_or_rebate == 0
