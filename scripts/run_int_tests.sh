d508e6633e18e1cb39db1ff749943d460c4a87f0
michaelhly <michaelhly@gmail.com>
2020-09-12 15:59:47 -0500

    Add run_int_tests script

---
+++ scripts/run_int_tests.sh
@@ -0,0 +1,16 @@
+if [ ! -d "serum-dex" ]; then
+    git clone https://github.com/serum-community/serum-dex.git
+fi
+
+cd serum-dex
+docker-compose up -d
+./do.sh build dex
+solana airdrop 10000
+DEX_PROGRAM_ID="$(solana deploy --use-deprecated-loader dex/target/bpfel-unknown-unknown/release/serum_dex.so | jq .programId -r)"
+echo DEX_PROGRAM_ID: $DEX_PROGRAM_ID
+cd crank
+cargo run -- l pyserum-setup ~/.config/solana/id.json $DEX_PROGRAM_ID
+echo "dex_program_id: $DEX_PROGRAM_ID" &> crank.log
+cp crank.log ../../tests
+cd ../..
+cat tests/crank.log
