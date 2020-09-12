d7c0a46badd1716c46d37ce0ddf517d4440f2c65
michaelhly <michaelhly@gmail.com>
2020-09-12 17:25:12 -0500

    Add integration tests

---
+++ tests/integration/test_bootstrap.py
@@ -0,0 +1,22 @@
+import pytest
+
+from solana.publickey import PublicKey
+
+
+@pytest.mark.integration
+def test_serum_dex(serum_dex):
+    """Make sure serum_dex fixture is populated with public keys."""
+    assert isinstance(serum_dex["coin_mint"], PublicKey)
+    assert isinstance(serum_dex["pc_mint"], PublicKey)
+    assert isinstance(serum_dex["market"], PublicKey)
+    assert isinstance(serum_dex["req_q"], PublicKey)
+    assert isinstance(serum_dex["event_q"], PublicKey)
+    assert isinstance(serum_dex["bids"], PublicKey)
+    assert isinstance(serum_dex["asks"], PublicKey)
+    assert isinstance(serum_dex["coin_vault"], PublicKey)
+    assert isinstance(serum_dex["pc_vault"], PublicKey)
+    assert isinstance(serum_dex["vault_signer_key"], PublicKey)
+    assert isinstance(serum_dex["wallet"], PublicKey)
+    assert isinstance(serum_dex["bid_account"], PublicKey)
+    assert isinstance(serum_dex["ask_account"], PublicKey)
+    assert isinstance(serum_dex["dex_program_id"], PublicKey)
