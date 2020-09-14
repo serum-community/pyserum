deed662168e58a8f3dcc78b494fdbe7734313289
michaelhly <michaelhly@gmail.com>
2020-09-13 20:26:30 -0500

    Download dex binary

--- scripts/run_int_tests.sh
+++ scripts/run_int_tests.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+os_type=""
+
+if [[ $OSTYPE == "linux-gnu"* ]]; then
+    os_type="linux"
+elif [[ $OSTYPE == "darwin"* ]]; then
+    os_type="darwin"
+else
+    echo $OSTYPE is not supported
+    exit 1
+fi
+
 if [ ! -d "serum-dex" ]; then
     git clone https://github.com/serum-community/serum-dex.git
 fi
@@ -9,10 +20,9 @@ fi
 cd serum-dex
 docker-compose up -d
 solana config set --url "http://localhost:8899"
-./do.sh build dex
-sleep 1.5
+curl -s -L "https://github.com/serum-community/serum-dex/releases/download/refs%2Fheads%2Fmaster/serum_dex-$os_type.so" > serum_dex.so
 solana airdrop 10000
-DEX_PROGRAM_ID="$(solana deploy --use-deprecated-loader dex/target/bpfel-unknown-unknown/release/serum_dex.so | jq .programId -r)"
+DEX_PROGRAM_ID="$(solana deploy --use-deprecated-loader serum_dex.so | jq .programId -r)"
 echo DEX_PROGRAM_ID: $DEX_PROGRAM_ID
 cd crank
 cargo run -- l pyserum-setup ~/.config/solana/id.json $DEX_PROGRAM_ID
