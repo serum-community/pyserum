6eb4a33e24b386905fccb9f8d348ecf7b9604605
michaelhly <michaelhly@gmail.com>
2020-09-13 21:01:12 -0500

    Udpate script and workflow

--- scripts/run_int_tests.sh
+++ scripts/run_int_tests.sh
@@ -16,6 +16,13 @@ fi
 docker-compose up -d
 solana config set --url "http://localhost:8899"
 curl -s -L "https://github.com/serum-community/serum-dex/releases/download/refs%2Fheads%2Fmaster/serum_dex-$os_type.so" > serum_dex.so
+if [[ $(which solana) == "" ]]; then
+    echo Installing Solana tool suite
+    curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v1.3.9/install/solana-install-init.sh | sh -s - v1.3.9
+    export PATH="/home/runner/.local/share/solana/install/active_release/bin:$PATH"
+    echo Generating keypair
+    solana-keygen new -o ~/.config/solana/id.json --no-passphrase --silent
+fi
 sleep 1
 solana airdrop 10000
 DEX_PROGRAM_ID="$(solana deploy --use-deprecated-loader serum_dex.so | jq .programId -r)"
