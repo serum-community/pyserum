5671e7b5dadc27180376f2a179e290adde245cb9
Leonard Ge <gw980531@gmail.com>
2020-09-17 15:24:27 +0800

    Reverted bootstrap_dex change.

--- scripts/bootstrap_dex.sh
+++ scripts/bootstrap_dex.sh
@@ -14,21 +14,21 @@ else
 fi
 
 docker-compose up -d
-# if ! hash solana 2>/dev/null; then
-#     echo Installing Solana tool suite...
-#     curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v1.3.9/install/solana-install-init.sh | sh -s - v1.3.9
-#     export PATH="/home/runner/.local/share/solana/install/active_release/bin:$PATH"
-#     echo Generating keypair...
-#     solana-keygen new -o ~/.config/solana/id.json --no-passphrase --silent
-# fi
+if ! hash solana 2>/dev/null; then
+    echo Installing Solana tool suite...
+    curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v1.3.9/install/solana-install-init.sh | sh -s - v1.3.9
+    export PATH="/home/runner/.local/share/solana/install/active_release/bin:$PATH"
+    echo Generating keypair...
+    solana-keygen new -o ~/.config/solana/id.json --no-passphrase --silent
+fi
 solana config set --url "http://localhost:8899"
-# curl -s -L "https://github.com/serum-community/serum-dex/releases/download/refs%2Fheads%2Fmaster/serum_dex-$os_type.so" > serum_dex.so
-sleep 10
+curl -s -L "https://github.com/serum-community/serum-dex/releases/download/refs%2Fheads%2Fmaster/serum_dex-$os_type.so" > serum_dex.so
+sleep 1
 solana airdrop 10000
 DEX_PROGRAM_ID="$(solana deploy --use-deprecated-loader serum_dex.so | jq .programId -r)"
 echo DEX_PROGRAM_ID: "$DEX_PROGRAM_ID"
-# curl -s -L "https://github.com/serum-community/serum-dex/releases/download/refs%2Fheads%2Fmaster/crank-$os_type" > crank
-# chmod +x crank
+curl -s -L "https://github.com/serum-community/serum-dex/releases/download/refs%2Fheads%2Fmaster/crank-$os_type" > crank
+chmod +x crank
 ./crank l pyserum-setup ~/.config/solana/id.json "$DEX_PROGRAM_ID"
 
 echo "dex_program_id: $DEX_PROGRAM_ID" >> crank.log
