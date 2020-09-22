0f0da2af30d517ce57e1d7032b77e64a956f2fbd
Leonard Ge <gw980531@gmail.com>
2020-09-22 15:12:12 +0800

    Fixed a nasty bug!

--- src/instructions.py
+++ src/instructions.py
@@ -405,7 +405,7 @@ def cancel_order(params: CancelOrderParams) -> TransactionInstruction:
                 instruction_type=InstructionType.CancelOrder,
                 args=dict(
                     side=params.side,
-                    order_id=params.order_id,
+                    order_id=params.order_id.to_bytes(32, byteorder='little'),
                     open_orders=bytes(params.open_orders),
                     open_orders_slot=params.open_orders_slot,
                 ),
--- src/market/_internal/queue.py
+++ src/market/_internal/queue.py
@@ -2,6 +2,8 @@ import math
 from enum import IntEnum
 from typing import List, Optional, Sequence, Tuple, Union, cast
 
+from solana.publickey import PublicKey
+
 from construct import Container  # type: ignore
 
 from ..._layouts.queue import EVENT_LAYOUT, QUEUE_HEADER_LAYOUT, REQUEST_LAYOUT
@@ -18,7 +20,7 @@ def __from_bytes(
 ) -> Tuple[Container, List[Union[Event, Request]]]:
     header = QUEUE_HEADER_LAYOUT.parse(buffer)
     layout_size = EVENT_LAYOUT.sizeof() if queue_type == QueueType.Event else REQUEST_LAYOUT.sizeof()
-    alloc_len = math.floor(len(buffer) - QUEUE_HEADER_LAYOUT.sizeof() / layout_size)
+    alloc_len = math.floor((len(buffer) - QUEUE_HEADER_LAYOUT.sizeof()) / layout_size)
     nodes: List[Union[Event, Request]] = []
     if history:
         for i in range(min(history, alloc_len)):
@@ -37,6 +39,30 @@ def __parse_queue_item(buffer: Sequence[int], queue_type: QueueType) -> Union[Ev
     layout = EVENT_LAYOUT if queue_type == QueueType.Event else REQUEST_LAYOUT
     parsed_item = layout.parse(buffer)
     parsed_item.pop("_io")  # Hack: Drop BytesIO object to fit kwargs into Event/Request object.
+    if queue_type == QueueType.Event:  # pylint: disable=no-else-return
+        return Event(
+            event_flags=parsed_item.event_flags,
+            open_order_slot=parsed_item.open_order_slot,
+            fee_tier=parsed_item.fee_tier,
+            native_quantity_released=parsed_item.native_quantity_released,
+            native_quantity_paid=parsed_item.native_quantity_paid,
+            native_fee_or_rebate=parsed_item.native_fee_or_rebate,
+            order_id=int.from_bytes(parsed_item.order_id, "little"),
+            public_key=PublicKey(parsed_item.public_key),
+            client_order_id=parsed_item.client_order_id,
+        )
+    else:
+        return Request(
+            request_flags=parsed_item.request_flags,
+            open_order_slot=parsed_item.open_order_slot,
+            fee_tier=parsed_item.fee_tier,
+            max_base_size_or_cancel_id=parsed_item.max_base_size_or_cancel_id,
+            native_quote_quantity_locked=parsed_item.native_quote_quantity_locked,
+            order_id=int.from_bytes(parsed_item.order_id, "little"),
+            open_orders=PublicKey(parsed_item.open_orders),
+            client_order_id=parsed_item.client_order_id,
+        )
+
     return Event(**parsed_item) if queue_type == QueueType.Event else Request(**parsed_item)
 
 
