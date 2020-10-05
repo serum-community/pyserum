b3dd716a521b4b7ac685d0e05cc146b81e70b8ff
michaelhly <michaelhly@gmail.com>
2020-10-05 12:55:38 -0500

    Use TxOpts

--- pyserum/market/market.py
+++ pyserum/market/market.py
@@ -7,6 +7,7 @@ from typing import List
 from solana.account import Account
 from solana.publickey import PublicKey
 from solana.rpc.api import Client
+from solana.rpc.types import TxOpts
 from solana.system_program import CreateAccountParams, create_account
 from solana.sysvar import SYSVAR_RENT_PUBKEY
 from solana.transaction import Transaction, TransactionInstruction
@@ -38,12 +39,8 @@ class Market:
         self,
         conn: Client,
         market_state: MarketState,
-        opts: t.MarketOpts = t.MarketOpts(),
     ) -> None:
-        self._skip_preflight = opts.skip_preflight
-        self._confirmations = opts.confirmations
         self._conn = conn
-
         self.state = market_state
 
     @staticmethod
@@ -52,11 +49,10 @@ class Market:
         conn: Client,
         market_address: PublicKey,
         program_id: PublicKey = instructions.DEFAULT_DEX_PROGRAM_ID,
-        opts: t.MarketOpts = t.MarketOpts(),
     ) -> Market:
         """Factory method to create a Market."""
         market_state = MarketState.load(conn, market_address, program_id)
-        return Market(conn, market_state, opts)
+        return Market(conn, market_state)
 
     def support_srm_fee_discounts(self) -> bool:
         raise NotImplementedError("support_srm_fee_discounts not implemented")
@@ -145,6 +141,7 @@ class Market:
         limit_price: int,
         max_quantity: int,
         client_id: int = 0,
+        opts: TxOpts = TxOpts(),
     ):  # TODO: Add open_orders_address_key param and fee_discount_pubkey
         transaction = Transaction()
         signers: List[Account] = [owner]
@@ -224,7 +221,7 @@ class Market:
                 )
             )
         # TODO: extract `make_place_order_transaction`.
-        return self._send_transaction(transaction, *signers)
+        return self._conn.send_transaction(transaction, *signers, opts=opts)
 
     @staticmethod
     def _get_lamport_need_for_sol_wrapping(
@@ -275,10 +272,11 @@ class Market:
             )
         )
 
-    def cancel_order_by_client_id(self, owner: Account, open_orders_account: PublicKey, client_id: int) -> str:
-        txs = Transaction()
-        txs.add(self.make_cancel_order_by_client_id_instruction(owner, open_orders_account, client_id))
-        return self._send_transaction(txs, owner)
+    def cancel_order_by_client_id(
+        self, owner: Account, open_orders_account: PublicKey, client_id: int, opts: TxOpts = TxOpts()
+    ) -> str:
+        txs = Transaction().add(self.make_cancel_order_by_client_id_instruction(owner, open_orders_account, client_id))
+        return self._conn.send_transaction(txs, owner, opts=opts)
 
     def make_cancel_order_by_client_id_instruction(
         self, owner: Account, open_orders_account: PublicKey, client_id: int
@@ -294,13 +292,13 @@ class Market:
             )
         )
 
-    def cancel_order(self, owner: Account, order: t.Order) -> str:
+    def cancel_order(self, owner: Account, order: t.Order, opts: TxOpts = TxOpts()) -> str:
         txn = Transaction().add(self.make_cancel_order_instruction(owner.public_key(), order))
-        return self._send_transaction(txn, owner)
+        return self._conn.send_transaction(txn, owner, opts=opts)
 
-    def match_orders(self, fee_payer: Account, limit: int) -> str:
+    def match_orders(self, fee_payer: Account, limit: int, opts: TxOpts = TxOpts()) -> str:
         txn = Transaction().add(self.make_match_orders_instruction(limit))
-        return self._send_transaction(txn, fee_payer)
+        return self._conn.send_transaction(txn, fee_payer, opts=opts)
 
     def make_cancel_order_instruction(self, owner: PublicKey, order: t.Order) -> TransactionInstruction:
         params = instructions.CancelOrderParams(
@@ -336,14 +334,6 @@ class Market:
         base_wallet: PublicKey,
         quote_wallet: PublicKey,
         referrer_quote_wallet: PublicKey,
+        opts: TxOpts = TxOpts(),
     ) -> str:
         raise NotImplementedError("settle_funds not implemented")
-
-    def _send_transaction(self, transaction: Transaction, *signers: Account) -> str:
-        res = self._conn.send_transaction(transaction, *signers, skip_preflight=self._skip_preflight)
-        if self._confirmations > 0:
-            self.logger.warning("Cannot confirm transaction yet.")
-        signature = res.get("result")
-        if not signature:
-            raise Exception("Transaction not sent successfully")
-        return str(signature)
