d46bd6ac03c1f1ab6908887b1c9390ca8d8ba546
michaelhly <michaelhly@gmail.com>
2020-10-29 17:10:59 -0500

    Revert layout changes

--- pyserum/_layouts/open_orders.py
+++ pyserum/_layouts/open_orders.py
@@ -19,20 +19,3 @@ OPEN_ORDERS_LAYOUT = cStruct(
     "referrer_rebate_accrued" / Int64ul,
     Padding(7),
 )
-
-OPEN_ORDERS_LAYOUT_V2 = cStruct(
-    Padding(5),
-    "account_flags" / ACCOUNT_FLAGS_LAYOUT,
-    "market" / Bytes(32),
-    "owner" / Bytes(32),
-    "base_token_free" / Int64ul,
-    "base_token_total" / Int64ul,
-    "quote_token_free" / Int64ul,
-    "quote_token_total" / Int64ul,
-    "free_slot_bits" / Bytes(16),
-    "is_bid_bits" / Bytes(16),
-    "orders" / Bytes(16)[128],
-    "client_ids" / Int64ul[128],
-    "referrer_rebates_accrued" / Int64ul,
-    Padding(7),
-)
