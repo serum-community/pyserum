4524638294aa02c7e00effe6aacacf2d71c4cc38
kevinheavey <kevinheavey123@gmail.com>
2021-08-09 16:19:07 +0100

    use correct solana version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=[
         "construct>=2.10.56, <3.0.0",
         "construct-typing>=0.5.1, <1.0.0",
-        "solana>=0.11.2, <1.0.0",
+        "solana>=0.11.3, <1.0.0",
     ],
     python_requires=">=3.7, <4",
     license="MIT",
