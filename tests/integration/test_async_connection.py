955426580f12502d4ab940bc61ccf4db5adfb947
kevinheavey <kevinheavey123@gmail.com>
2021-08-11 13:30:36 +0100

    add async tests

---
+++ tests/integration/test_async_connection.py
@@ -0,0 +1,23 @@
+import pytest
+import httpx
+
+from pyserum.async_connection import get_live_markets, get_token_mints
+from pyserum.market.types import MarketInfo, TokenInfo
+
+
+@pytest.mark.integration
+@pytest.mark.asyncio
+async def test_get_live_markets():
+    """Test get_live_markets."""
+    async with httpx.AsyncClient() as client:
+        resp = await get_live_markets(client)
+    assert all(isinstance(market_info, MarketInfo) for market_info in resp)
+
+
+@pytest.mark.integration
+@pytest.mark.asyncio
+async def test_get_token_mints():
+    """Test get_token_mints."""
+    async with httpx.AsyncClient() as client:
+        resp = await get_token_mints(client)
+    assert all(isinstance(token_info, TokenInfo) for token_info in resp)
--- tests/integration/test_connection.py
+++ tests/integration/test_connection.py
@@ -1,12 +1,16 @@
+import pytest
+
 from pyserum.connection import get_live_markets, get_token_mints
 from pyserum.market.types import MarketInfo, TokenInfo
 
 
+@pytest.mark.integration
 def test_get_live_markets():
     """Test get_live_markets."""
     assert all(isinstance(market_info, MarketInfo) for market_info in get_live_markets())
 
 
+@pytest.mark.integration
 def test_get_token_mints():
     """Test get_token_mints."""
     assert all(isinstance(token_info, TokenInfo) for token_info in get_token_mints())
