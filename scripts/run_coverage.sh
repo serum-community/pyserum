513e97a80eeb31d941c02e10dd335b8f2b91dd77
kevinheavey <kevinheavey123@gmail.com>
2021-08-12 02:04:46 +0100

    fix coverage for multi test

--- scripts/run_coverage.sh
+++ scripts/run_coverage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pipenv run pytest -m  "not integration and not async_integration" --cov=./ --cov-report=xml --cov-append
+
 bash scripts/bootstrap_dex.sh
 
 wait_time=20
@@ -7,9 +9,18 @@ echo "Waiting $wait_time seconds to make sure the market has started"
 sleep $wait_time
 
 
+pipenv run pytest -m integration --cov=./ --cov-report=xml --cov-append)
+
+bash scripts/clean_up.sh
+
+bash scripts/bootstrap_dex.sh
+
+wait_time=20
+echo "Waiting $wait_time seconds to make sure the market has started"
+sleep $wait_time
 
 exit_code=1
-if (pipenv run pytest --cov=./ --cov-report=xml --cov-append); then
+if (pipenv run pytest -m async_integration --cov=./ --cov-report=xml --cov-append); then
   echo "The script ran ok"
   exit_code=0
 fi
