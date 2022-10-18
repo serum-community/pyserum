dd7deb8d2b1a8857d32c630b51a20f63f7055ddd
Ennio Nasca <nascaennio@gmail.com>
2022-10-18 17:45:45 +0200

    chore: use latest validator version

--- scripts/bootstrap_dex.sh
+++ scripts/bootstrap_dex.sh
@@ -16,7 +16,7 @@ fi
 
 if ! hash solana 2>/dev/null; then
     echo Installing Solana tool suite ...
-    curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v1.5.8/install/solana-install-init.sh | SOLANA_RELEASE=v1.5.8 sh -s - v1.5.8
+    curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v1.13.3/install/solana-install-init.sh | SOLANA_RELEASE=v1.13.3 sh -s - v1.13.3
     export PATH="/home/runner/.local/share/solana/install/active_release/bin:$PATH"
     if [ ! -f ~/.config/solana/id.json ]; then
         echo Generating keypair ...
@@ -29,7 +29,7 @@ solana config set --url "http://127.0.0.1:8899"
 curl -s -L "https://github.com/serum-community/serum-dex/releases/download/v2/serum_dex-$os_type.so" > serum_dex.so
 sleep 1
 solana airdrop 10000
-DEX_PROGRAM_ID="$(solana deploy --use-deprecated-loader serum_dex.so | jq .programId -r)"
+DEX_PROGRAM_ID="$(solana deploy serum_dex.so --output json | jq .programId -r)"
 echo DEX_PROGRAM_ID: "$DEX_PROGRAM_ID"
 curl -s -L "https://github.com/serum-community/serum-dex/releases/download/v2/crank-$os_type" > crank
 chmod +x crank
